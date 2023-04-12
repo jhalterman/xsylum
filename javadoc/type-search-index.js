b3f4b7fe5f071e2edc8ca616b6574d6cabf0e14b
Jonathan Halterman <jhalterman@gmail.com>
2023-04-11 18:51:37 -0700

    Updated JavaDocs

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"net.jodah.xsylum","l":"XmlDocument"},{"p":"net.jodah.xsylum","l":"XmlElement"},{"p":"net.jodah.xsylum","l":"XmlSearchable"},{"p":"net.jodah.xsylum","l":"Xsylum"},{"p":"net.jodah.xsylum","l":"XsylumException"}]
